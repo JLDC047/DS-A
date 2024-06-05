2275da04f11bcced19c7e8b6fade01fef84e4eff
JLDC047 <166645969+JLDC047@users.noreply.github.com>
2024-06-05 18:23:47 +0100

    Update InfixToPostfix.py

--- InfixToPostfix.py
+++ InfixToPostfix.py
@@ -1,6 +1,6 @@
 class Stack():
-    def __init__(self, capicity):
-        self.capicity = capicity
+    def __init__(self, capacity):
+        self.capicity = capacity
         self.stack = []
         self.stackOverflow = False
   
@@ -24,12 +24,43 @@ class Stack():
             return False
         else:
             return True
+        
+    def length(self):
+        return len(self.stack)
 
     def show_stack(self):
         print(self.stack)
 
-precendence = {"+":1, "-":1, "*":2, "/":2, "^":3}
+precendence = {"+":1, "-":1, "*":2, "/":2, "^":3, "(":0, ")":0}
 
 def infixToPostfix(expression):
-    stack = Stack(15)
+    operators = Stack(15)
     postfixString = ""
+    for character in expression:
+        if character not in precendence:
+            postfixString += character 
+        else:
+            if character == "(":
+                operators.push(character)
+            elif character == ")":
+                lastOperator = operators.pop()
+                #print(lastOperator) 
+                while not operators.is_empty() and lastOperator != "(":
+                        postfixString += lastOperator
+                        lastOperator = operators.pop()
+                        #print(lastOperator)
+            else:
+                while not operators.is_empty() and precendence[character] <= precendence[operators.top()] and operators.top() != "(":
+                    print(precendence[character])
+                    print(precendence[operators.top()])
+                    postfixString += operators.pop()                  
+                operators.push(character)    
+        #print(postfixString)            
+        #print(operators.stack)    
+    while not operators.is_empty():
+        postfixString += operators.pop()              
+    return postfixString                 
+    
+
+print(infixToPostfix("c+d/f-(a+b-d)+e"))
+
